809b248cf5d649440d2a146f0ede0f6d76ecbaa7
ImaneBenzegunine <imane.benzegunine@gmail.com>
2025-03-15 22:47:52 +0000

    Clean data

---
+++ Scripts/paths.py
@@ -0,0 +1,8 @@
+import os
+
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
+PATH_AUTO_CSV = os.path.join(BASE_DIR, "data",  "smart_logistics_dataset.csv")
+
+
+#PATH_PROCESSED_CSV = os.path.join(BASE_DIR, "data", "outputTransfomed", "processed.csv")
