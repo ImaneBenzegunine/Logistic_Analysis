00d70c2ca78ae324199d250f85cb56ddf520c462
ImaneBenzegunine <imane.benzegunine@gmail.com>
2025-04-21 00:10:44 +0100

    data is prepared for visualisation

--- Scripts/paths.py
+++ Scripts/paths.py
@@ -5,4 +5,9 @@ BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 PATH_AUTO_CSV = os.path.join(BASE_DIR, "data",  "smart_logistics_dataset.csv")
 
 
-#PATH_PROCESSED_CSV = os.path.join(BASE_DIR, "data", "outputTransfomed", "processed.csv")
+PATH_CLEAN_CSV = os.path.join(BASE_DIR, "data",  "clean_data.csv")
+
+
+
+
+PATH_PREPARED_CSV = os.path.join(BASE_DIR, "data", "prepared_data.csv")
